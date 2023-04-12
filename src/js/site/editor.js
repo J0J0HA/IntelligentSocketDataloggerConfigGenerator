41880c28e30573c736bc4d06c3a1749ea1e8e60e
JoJoJux <80523427+J0J0HA@users.noreply.github.com>
2023-04-12 19:27:59 +0200

    Upgrade to 1.6 Config

--- src/js/site/editor.js
+++ src/js/site/editor.js
@@ -5,10 +5,10 @@ const devices = {
 };
 
 const loglevelicon = {
-    "debug": '<i class="fa-solid fa-bug"></i>',
-    "info": '<i class="fa-solid fa-circle-info"></i>',
-    "warning": '<i class="fa-solid fa-triangle-exclamation"></i>',
-    "error": '<i class="fa-solid fa-square-xmark"></i>'
+    "debug": 'terminal',
+    "info": 'info',
+    "warning": 'alert-triangle',
+    "error": 'x-square'
 }
 
 if (!String.prototype.padStart) {
@@ -19,7 +19,7 @@ if (!String.prototype.padStart) {
 
 
 try {
-    var current = JSON.parse(localStorage.getItem("recentlyThisHappend")) || {
+    var current = JSON.parse(localStorage.getItem("1.6-config")) || {
         general: {
             log_level: "info",
             cost_calc_request_time: "00:00",
@@ -41,6 +41,15 @@ try {
 var edit_name_no_alert = "NGGYU";
 
 $(() => {
+    $("#cost_calc_toy").datepicker({
+        defaultDate: 0,
+        changeYear: false,
+        dateFormat: "dd.mm"
+    }).focus(function() {
+        //$(".ui-datepicker-year").hide()
+    })
+
+
     formula.autoassign();
     formula.init();
 
@@ -50,7 +59,9 @@ $(() => {
         const data = await $("#config-file").prop('files')[0].text();
         current.general = JSON.parse(data)["general"];
         $("#log_level").val(current.general.log_level);
-        $("#cost_calc_request_time").val(current.general.cost_calc_request_time);
+        $("#cost_calc_tod").val(current.general.cost_calc_request_time_daily);
+        $("#cost_calc_dom").val(current.general.cost_calc_request_time_monthly);
+        $("#cost_calc_toy").val(current.general.cost_calc_request_time_yearly);
         $("#price_kwh").val(current.general.price_kwh);
         generate();
     })
@@ -76,13 +87,14 @@ $(() => {
         $("#edit-type").text("shelly:plug-s");
         $("#edit-ip").val("192.168.178.200");
         $("#edit-update_time").val("30");
-        $("#dayofmonth").val("1");
-        $("#dayofyear").val("1");
-        $("#monthofyear").val("1");
 
         formula.setOptionValue("edit-cost_calc_day", true)
         formula.setOptionValue("edit-cost_calc_month", false)
         formula.setOptionValue("edit-cost_calc_year", false)
+
+        formula.setOptionValue("edit-poc_day", true)
+        formula.setOptionValue("edit-poc_month", false)
+        formula.setOptionValue("edit-poc_year", false)
 
         $("#edit-box").css("display", "block");
     })
@@ -99,21 +111,22 @@ $(() => {
         if (edit_name_no_alert != "NGGYU") {
             delete current.devices[edit_name_no_alert];
         }
-        cost_calc = {}
-        if (formula.getOptionValue("edit-cost_calc_day")) {
-            cost_calc["cost_calc_day"] = true;
-        }
-        if (formula.getOptionValue("edit-cost_calc_month")) {
-            cost_calc["cost_calc_month"] = $("#dayofmonth").val().padStart(2, "0");
-        }
-        if (formula.getOptionValue("edit-cost_calc_year")) {
-            cost_calc["cost_calc_year"] = $("#dayofyear").val().padStart(2, "0") + "." + $("#monthofyear").val().padStart(2, "0");
-        }
         current.devices[ename] = {
             type: $("#edit-type").text(),
             ip: $("#edit-ip").val(),
             update_time: $("#edit-update_time").val(),
-            ...cost_calc
+            cost_calculation: {
+                daily: formula.getOptionValue("edit-cost_calc_day"),
+                monthly: formula.getOptionValue("edit-cost_calc_month"),
+                yearly: formula.getOptionValue("edit-cost_calc_year")
+            },
+            power_on_counter: {
+                daily: formula.getOptionValue("edit-poc_day"),
+                monthly: formula.getOptionValue("edit-poc_month"),
+                yearly: formula.getOptionValue("edit-poc_year"),
+                on_threshold: 2,
+                off_threshold: 1
+            }
         }
         $("#edit-box").css("display", "none");
         edit_name_no_alert = "NGGYU"
@@ -128,12 +141,13 @@ $(() => {
 
     setInterval(() => {
         // Transfer Time
-        $("[name=filltime]").text($("#cost_calc_request_time").val())
-        $("#selected_icon").html(loglevelicon[$("#log_level").val()])
+        $("[name=filltime]").text($("#cost_calc_tod").val())
+        $("#selected_icon").html(feather.icons[loglevelicon[$("#log_level").val()]].toSvg())
 
         // Check values to be valid here!
-        if (!$("#cost_calc_request_time").val().match(/^\d\d\:\d\d$/g)) $("#cost_calc_request_time").addClass("f-tag-wrong")
-        else $("#cost_calc_request_time").removeClass("f-tag-wrong")
+        matchres = $("#cost_calc_tod").val().match(/^(\d\d?)\:(\d\d?)$/)
+        if (!matchres || matchres[1] < 0 || matchres[1] > 12 || matchres[2] < 0 || matchres[2] > 60) $("#cost_calc_tod").addClass("f-tag-wrong")
+        else $("#cost_calc_tod").removeClass("f-tag-wrong")
         if (!$("#price_kwh").val().match(/^[\d\.]+$/g) || (parseFloat($("#price_kwh").val()) <= 0)) $("#price_kwh").addClass("f-tag-wrong")
         else $("#price_kwh").removeClass("f-tag-wrong")
         if (!$("#edit-name").val().match(/^.+$/g) || (($("#edit-name").val() != edit_name_no_alert) && ($("#edit-name").val() in current.devices))) $("#edit-name").addClass("f-tag-wrong")
@@ -142,12 +156,11 @@ $(() => {
         else $("#edit-ip").removeClass("f-tag-wrong")
         if (!$("#edit-update_time").val().match(/^\d+$/g) || (parseInt($("#edit-update_time").val()) <= 0)) $("#edit-update_time").addClass("f-tag-wrong")
         else $("#edit-update_time").removeClass("f-tag-wrong")
-        if (!$("#dayofmonth").val().match(/^\d+$/g) || (parseInt($("#dayofmonth").val()) <= 0) || (parseInt($("#dayofmonth").val()) > 31)) $("#dayofmonth").addClass("f-tag-wrong")
-        else $("#dayofmonth").removeClass("f-tag-wrong")
-        if (!$("#dayofyear").val().match(/^\d+$/g) || (parseInt($("#dayofyear").val()) <= 0) || (parseInt($("#dayofyear").val()) > 31)) $("#dayofyear").addClass("f-tag-wrong")
-        else $("#dayofyear").removeClass("f-tag-wrong")
-        if (!$("#monthofyear").val().match(/^\d+$/g) || (parseInt($("#monthofyear").val()) <= 0) || (parseInt($("#monthofyear").val()) > 12)) $("#monthofyear").addClass("f-tag-wrong")
-        else $("#monthofyear").removeClass("f-tag-wrong")
+        if (!$("#cost_calc_dom_m").val().match(/^\d+$/g) || (parseInt($("#cost_calc_dom_m").val()) <= 0) || (parseInt($("#cost_calc_dom_m").val()) > 31)) $("#cost_calc_dom_m").addClass("f-tag-wrong")
+        else $("#cost_calc_dom_m").removeClass("f-tag-wrong")
+        matchres = $("#cost_calc_toy").val().match(/^(\d+)\.(\d+)$/);
+        if (!matchres || matchres[1] < 1 || matchres[1] > 31 || matchres[2] < 1 || matchres[2] > 12) $("#cost_calc_toy").addClass("f-tag-wrong")
+        else $("#cost_calc_toy").removeClass("f-tag-wrong");
     }, 250)
 
     list_devices();
@@ -157,42 +170,50 @@ function list_devices() {
     var html = "";
     for (const dname in current.devices) {
         dval = current.devices[dname];
+        ccval = dval["cost_calculation"]
         html += "<tr>";
         html += '<td>' + dname + "</td>";
-        html += '<td><i class="fa-solid fa-bullseye"></i> ' + (devices[dval["type"]] || dval["type"] || "undefined") + "</td>";
-        html += '<td><i class="fa-solid fa-display"></i> ' + dval["ip"] + "</td>";
-        html += '<td><i class="fa-regular fa-clock"></i> ' + dval["update_time"] + "sec</td>";
-        html += '<td>' + (dval["cost_calc_day"] ? '<i class="fa-solid fa-check"></i>' : '<i class="fa-solid fa-xmark"></i>') + '<br>' + (dval["cost_calc_month"] ? ('<i class="fa-solid fa-calendar-days"></i> ' + dval["cost_calc_month"] + ".") : '<i class="fa-solid fa-xmark"></i>') + '<br>' + (dval["cost_calc_year"] ? ('<i class="fa-solid fa-calendar-days"></i> ' + dval["cost_calc_year"] + ".") : '<i class="fa-solid fa-xmark"></i>') + '</td>';
-        html += '<td><button onclick="edit_device(\'' + dname + '\')"><i class="fa-solid fa-gear"></i></button><button onclick="delete_device(\'' + dname + '\')"><i class="fa-solid fa-trash-can"></i></button></td>';
+        html += '<td><i data-feather="target"></i> ' + (devices[dval["type"]] || dval["type"] || "undefined") + "</td>";
+        html += '<td><i data-feather="monitor"></i> ' + dval["ip"] + "</td>";
+        html += '<td><i data-feather="clock"></i> ' + dval["update_time"] + "sec</td>";
+        html += '<td>' + (ccval["daily"] ? '<i data-feather="check"></i>' : '<i data-feather="x"></i>') + '<br>' + (ccval["monthly"] ? '<i data-feather="check"></i>' : '<i data-feather="x"></i>') + '<br>' + (ccval["yearly"] ? '<i data-feather="check"></i>' : '<i data-feather="x"></i>') + '</td>';
+        ccval = dval["power_on_counter"]
+        html += '<td>' + (ccval["daily"] ? '<i data-feather="check"></i>' : '<i data-feather="x"></i>') + '<br>' + (ccval["monthly"] ? '<i data-feather="check"></i>' : '<i data-feather="x"></i>') + '<br>' + (ccval["yearly"] ? '<i data-feather="check"></i>' : '<i data-feather="x"></i>') + '</td>';
+        html += '<td><button onclick="edit_device(\'' + dname + '\')"><i data-feather="settings"></i></button><button onclick="delete_device(\'' + dname + '\')"><i data-feather="trash-2"></i></button></td>';
         html += "</tr>";
     }
     $("#devviebod").html(html || "<tr><td>No devices listed.</td></tr>");
     formula.reindex_static()
+    feather.replace()
 }
 
 function generate() {
     current.general.log_level = $("#log_level").val()
-    current.general.cost_calc_request_time = $("#cost_calc_request_time").val()
+    current.general.cost_calc_request_time_daily = $("#cost_calc_tod").val()
+    current.general.cost_calc_request_time_monthly = $("#cost_calc_dom_m").val()
+    current.general.cost_calc_request_time_yearly = $("#cost_calc_toy").val()
     current.general.price_kwh = parseFloat($("#price_kwh").val())
-    localStorage.setItem("recentlyThisHappend", JSON.stringify(current))
+    localStorage.setItem("1.6-config", JSON.stringify(current))
     return current;
 }
 
 function edit_device(dname) {
     edit_name_no_alert = dname;
     dval = current.devices[dname];
+    ccval = dval["cost_calculation"];
     const date = dval["cost_calc_year"]?.split(".")
-
+    
     $("#edit-name").val(dname);
     $("#edit-type").text(dval["type"] || "undefined");
     $("#edit-ip").val(dval["ip"]);
     $("#edit-update_time").val(dval["update_time"]);
-    $("#dayofmonth").val(dval["cost_calc_month"] || "1");
-    $("#dayofyear").val(date?.[0] || "1");
-    $("#monthofyear").val(date?.[1] || "1");
-    formula.setOptionValue("edit-cost_calc_day", dval["cost_calc_day"])
-    formula.setOptionValue("edit-cost_calc_month", dval["cost_calc_month"])
-    formula.setOptionValue("edit-cost_calc_year", dval["cost_calc_year"])
+    formula.setOptionValue("edit-cost_calc_day", ccval["daily"])
+    formula.setOptionValue("edit-cost_calc_month", ccval["monthly"])
+    formula.setOptionValue("edit-cost_calc_year", ccval["yearly"])
+    ccval = dval["power_on_counter"];
+    formula.setOptionValue("edit-poc_day", ccval["daily"])
+    formula.setOptionValue("edit-poc_month", ccval["monthly"])
+    formula.setOptionValue("edit-poc_year", ccval["yearly"])
 
     $("#edit-box").css("display", "block");
 }
